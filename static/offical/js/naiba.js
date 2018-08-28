7fddbd0402c03f9314db4e7706b4c34f8b9d7670
Naiba <1@5.nu>
2018-08-28 12:34:30 +0800

    用户登录注册找回密码

--- static/offical/js/naiba.js
+++ static/offical/js/naiba.js
@@ -28,6 +28,81 @@ $(document).ready(function () {
   })
 });
 
+function resetPassword() {
+  var what = $('div.container-item.is-active');
+  var btn = what.find('button.is-primary');
+  var err = what.find('div.notification');
+  var iptMail = what.find('input[name=mail]').val()
+  var iptVerify = what.find('input[name=verify]').val()
+  var iptPassword = what.find('input[name=password]').val()
+  //设置loading状态
+  btn.addClass('is-loading');
+  err.prop('style', '');
+  //表单校验
+  var errMsg = ''
+  if (iptMail.length < 6) {
+    errMsg += "邮箱格式不规范\n\n"
+  }
+  if (iptPassword.length < 6) {
+    errMsg += '密码不能少于6位\n\n'
+  }
+  if (iptVerify.length != 5) {
+    errMsg += '验证码不正确\n\n'
+  }
+  if (errMsg.length > 0) {
+    err.text(errMsg)
+    btn.removeClass('is-loading');
+    return
+  }
+  $.post('/reset_password', { mail: iptMail, password: iptPassword, verify: iptVerify }).
+    always(function () {
+      btn.removeClass('is-loading');
+    }).fail(function (resp) {
+      err.text(resp.responseText);
+    }).done(function (resp) {
+      window.location.href = "/dashboard"
+    })
+}
+
+function register(btn) {
+  var what = $('div.container-item.is-active');
+  var err = what.find('div.notification');
+  var iptMail = what.find('input[name=mail]').val()
+  var iptVerify = what.find('input[name=verify]').val()
+  var iptPassword = what.find('input[name=password]').val()
+  var iptRePassword = what.find('input[name=repassword]').val()
+  //设置loading状态
+  $(btn).addClass('is-loading');
+  err.prop('style', '');
+  //表单校验
+  var errMsg = ''
+  if (iptMail.length < 6) {
+    errMsg += "邮箱格式不规范\n\n"
+  }
+  if (iptPassword.length < 6) {
+    errMsg += '密码不能少于6位\n\n'
+  }
+  if (iptPassword !== iptRePassword) {
+    errMsg += '两次输入的密码不一致\n\n'
+  }
+  if (iptVerify.length != 5) {
+    errMsg += '验证码不正确\n\n'
+  }
+  if (errMsg.length > 0) {
+    err.text(errMsg)
+    $(btn).removeClass('is-loading');
+    return
+  }
+  $.post('/register', { mail: iptMail, password: iptPassword, verify: iptVerify }).
+    always(function () {
+      $(btn).removeClass('is-loading');
+    }).fail(function (resp) {
+      err.text(resp.responseText);
+    }).done(function (resp) {
+      window.location.href = "/dashboard"
+    })
+}
+
 function index_recaptcha_callback(gtoken) {
   $('div[class*="modal"][data-item="recaptcha"]').removeClass("is-active");
   var what = $('div.container-item.is-active');
@@ -37,34 +112,58 @@ function index_recaptcha_callback(gtoken) {
   btn.addClass('is-loading');
   //重置ReCaptcha
   grecaptcha.reset();
-  console.log(what.data('item'))
+
+  // 通用校验
+  var mail = what.find('input[name="mail"]').val();
+  if (mail.length == 0) {
+    btn.removeClass('is-loading');
+    err.prop('style', '');
+    err.text('邮箱不能为空');
+    return
+  }
+
   switch (what.data('item')) {
     case 1:
       //登录站点
-      break;
-    case 2:
-      //注册验证码
-      console.log('发送注册验证码')
-      var mail = what.find('input[name="mail"]').val();
-      if (mail.length == 0) {
+      var pass = what.find('input[name="password"]').val();
+      if (pass.length < 6) {
         btn.removeClass('is-loading');
         err.prop('style', '');
-        err.text('邮箱不能为空');
+        err.text('密码最少为6位');
         return
       }
-      $.post('/verify/mail', { type: 'reg', mail: mail }).always(function () {
+      $.post('/login', { mail: mail, password: pass, gresp: gtoken }).always(function () {
         btn.removeClass('is-loading');
       }).fail(function (resp) {
-        console.log(resp)
         err.prop('style', '');
         err.text(resp.responseText);
       }).done(function (resp) {
-        err.prop('style', '');
+        window.location.href = "/dashboard"
+      })
+      break;
+    case 2:
+      //注册验证码
+      $.post('/verify/mail', { type: 'reg', mail: mail, gresp: gtoken }).always(function () {
+        btn.removeClass('is-loading');
+      }).fail(function (resp) {
+        err.text(resp.responseText);
+      }).done(function (resp) {
         err.text('验证码发送成功');
+      }).always(function () {
+        err.prop('style', '');
       });
       break;
     case 3:
       //重置密码验证码
+      $.post('/verify/mail', { type: 'forget', mail: mail, gresp: gtoken }).always(function () {
+        btn.removeClass('is-loading');
+      }).fail(function (resp) {
+        err.text(resp.responseText);
+      }).done(function (resp) {
+        err.text('验证码发送成功');
+      }).always(function () {
+        err.prop('style', '');
+      });
       break;
   }
 }
